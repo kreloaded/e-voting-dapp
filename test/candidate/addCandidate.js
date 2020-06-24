aaa58a3bcf181bed18e8b46b8224c1400e45ba6a
kreloaded <kiran@ost.com>
2020-06-24 16:12:43 +0530

    added ganache script

---
+++ test/candidate/addCandidate.js
@@ -0,0 +1,38 @@
+const BN = require('bn.js');
+const { AccountProvider } = require('../test_lib/utils');
+
+const CandidateContract = artifacts.require('Candidate');
+const Utils = require('../test_lib/utils');
+
+contract('Candidate::addCandidate', async (accounts) => {
+  const accountProvider = new AccountProvider(accounts);
+
+  let candidateContract = await CandidateContract.new();
+  let candidateAddress = accountProvider.get();
+  let param = {};
+
+  beforeEach(async () => {
+    param = {
+      name: 'xyz',
+      nic: 'pqr',
+      party: 'klm'
+    };
+  });
+
+  contract('Positive Tests', async () => {
+    it('should pass when candidate is successfully added.', async () => {
+      await candidateContract.addCandidate(
+        param.name,
+        param.nic,
+        param.party,
+        { from: candidateAddress },
+      );
+
+      const candidate = await candidateContract.candidates.call(candidateAddress);
+      assert.isNotNull(
+        candidate,
+        'Candidate is not successfully added.',
+      );
+    });
+  });
+});
